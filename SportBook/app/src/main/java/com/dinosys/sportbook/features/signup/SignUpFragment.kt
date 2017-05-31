a94f27a5f553c705cc1268ca4f4d5809ff004e43
ducdino <duc.han@dinosys.edu.vn>
2017-05-31 15:49:26 +0700

    add signup screen

---
+++ SportBook/app/src/main/java/com/dinosys/sportbook/features/signup/SignUpFragment.kt
@@ -0,0 +1,7 @@
+package com.dinosys.sportbook.features.signup
+
+/**
+ * Created by hanth on 31/05/2017.
+ */
+
+class SignUpFragment
---
+++ SportBook/app/src/main/java/com/dinosys/sportbook/features/signup/SignUpViewModel.kt
@@ -0,0 +1,5 @@
+package com.dinosys.sportbook.features.signup
+
+/**
+ * Created by hanth on 31/05/2017.
+ */
